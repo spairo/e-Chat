e07a726dfac9b856b6dba1e2a372ba0afab65ddf
Luis Alberto <nelixhost@gmail.com>
2015-02-11 12:32:33 -0600

    Clearing main controllers
    Clearing main controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,4 @@
-'use strict';
-
-/**
+/*
 * @ngdoc overview
 * @name CamuApp
 * @description CamuApp 2.0
@@ -13,17 +11,24 @@
 'use strict';
 
   angular.module("app", ["ngRoute", "ui.router", "ngAnimate", "ui.bootstrap", "easypiechart", "mgo-angular-wizard", "textAngular", "ui.tree", "ngMap", "ngTagsInput", "app.ui.ctrls", "app.ui.directives", "app.ui.services", "app.controllers", "app.directives", "app.form.validation", "app.ui.form.ctrls", "app.ui.form.directives", "app.tables", "app.map", "app.task", "app.localization", "app.chart.ctrls", "app.chart.directives", "app.page.ctrls"]).config(["$routeProvider", function($routeProvider) {
-      
+
       return $routeProvider.when("/", {
         redirectTo: "/dashboard"
-      }).when("/dashboard", {
+      })
+      .when("/dashboard", {
         templateUrl: "views/dashboard.html"
-      }).when("/pages/signin", {
+      })
+      .when("/pages/signin", {
         templateUrl: "views/pages/signin.html"
-      }).when("/pages/blank", {
+      })
+      .when("/pages/blank", {
         templateUrl: "views/pages/blank.html"
-      }).otherwise({
+      })
+      .when('catalogs/services',{
+        templateUrl: 'views/services.html'
+      })
+      .otherwise({
         redirectTo: "/pages/signin"
       })
-      
+
   }])
